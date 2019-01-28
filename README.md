ec60470e9317d56aaafbd93cf96ba8dd8b5f98d0
okadath <viruz_3107@hotmail.com>
2019-01-27 19:09:49 -0600

    login

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-scraping
----
+# Scraping
+
  
     sudo apt-get update
     sudo apt-get install software-properties-common
@@ -10,24 +10,36 @@ scraping
     
     virtualenv --python=python3.6 venv
     source venv/bin/activate
-    
-    
+      
 instalar phantomjs:
 
     curl -sL https://deb.nodesource.com/setup_10.x | sudo bash -
     sudo apt install nodejs
 	sudo npm i -g install phantomjs-prebuilt --unsafe-perm
 
-corre mas rapido sin hacer el test unitario y sin firefox, sera un lio ir tomando las fotos y giandolo remotamente XD
+corre mas rapido sin hacer el test unitario y sin Firefox, sera un lio ir tomando las fotos y guiandolo remotamente XD
 
-para guardar en python :
-	
-	'folder/nombre.png'
+para guardar en python `folder/nombre.png`
 
-usar driver.quit() para evitar tener el navegador abierto en memoria
+usar `driver.quit()` para evitar tener el navegador abierto en memoria
 
 correr el server en segundo plano:
     
     nohup python -m http.server 7000 > my.log 2>&1 &
     ps -ef |grep python
     kill -9 <PI>
+
+descargar geckodriver actualizado para usarlo en ubuntu, descargar y descomprimir:
+
+    chmod +x geckodriver
+    export PATH=$PATH:/path.../geckodriver y agregarlo como variable al driver
+    o guardarlo en /usr/bin/
+
+usar polyglot
+
+
+
+
+
+
+
--- ph_1.py
+++ ph_1.py
@@ -1,9 +1,42 @@
 from selenium import webdriver
+from bs4 import BeautifulSoup
+import time
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 
-driver = webdriver.PhantomJS() # or add to your PATH
-driver.set_window_size(1024, 768) # optional
-driver.get('https://google.com/')
-driver.save_screenshot('capturas/screen.png') # save a screenshot to disk
-driver.quit()
+dcap = dict(DesiredCapabilities.PHANTOMJS)
+dcap["phantomjs.page.settings.userAgent"] = ("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.122 Safari/537.36")
+base_url = "https://twitter.com"
+
+def login(driver):
+	print("logeando")
+	driver.get(base_url)
+	time.sleep(1)
+	username = driver.find_element_by_name("session[username_or_email]")
+	password = driver.find_element_by_name("session[password]")
+	time.sleep(1)
+	username.send_keys("bototuit@mail.com")
+	password.send_keys("Qwerty35$")
+	time.sleep(1)
+	elem = driver.find_element_by_xpath("//input[@class='EdgeButton EdgeButton--secondary EdgeButton--medium submit js-submit'][@type='submit']")
+	elem.click()
+
+try:
+	print("1")
+	driver=webdriver.PhantomJS(service_args=["--load-images=no"])# or add to your PATH
+	# f_profile = webdriver.FirefoxProfile()
+	# #firefox_profile.set_preference('permissions.default.stylesheet', 2)
+	# f_profile.set_preference('permissions.default.image',2)
+	# driver = webdriver.Firefox(firefox_profile=f_profile)
+	print("2")
+	driver.set_window_size(1024, 768) # optional
+	# driver.get('https://google.com/')
+	login(driver)
+	driver.save_screenshot('capturas/screen.png')
+	
+except Exception as e:
+	print("error:",e)
+finally:
+	print("termino")
+	#driver.quit()
 
